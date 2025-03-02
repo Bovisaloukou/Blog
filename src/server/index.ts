aabf5a638c48315d26a0213bbb763fc190c4ae83
Bovis ALOUKOU <bovisaloukou@gmail.com>
2025-03-02 18:17:56 +0100

    modification des variables d'environnement

--- src/server/index.ts
+++ src/server/index.ts
@@ -18,8 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: [
-    'https://project-lime-zeta.vercel.app',
-    'http://localhost:5173'
+    process.env.CLIENT_URL || 'http://localhost:5173',
   ],
   credentials: true
 }));
